b27bf567cf3ab090c0604ee55701f72b8ad8f49e
Samuel Moss <146960635+Samuel-Moss@users.noreply.github.com>
2025-09-28 16:35:05 +0100

    Create dl-algorithm.py

---
+++ ai/dl-algorithm.py
@@ -0,0 +1,16 @@
+"""
+File: dl-algorithm.py
+Author: Sam Moss, Josh White, Jack Wainwright
+Created: 2025-09-28
+Last Updated: 2025-09-28
+
+Description:
+This is a placeholder file
+    
+Usage:
+    python dl-algorithm.py
+
+Notes:
+    - This is a placeholder file
+    - Need to decide on a deep learning algorithm
+"""
