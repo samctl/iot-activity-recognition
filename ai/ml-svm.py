4afb465f69fb2d638fbad7488c85283f2172390b
Samuel Moss <146960635+Samuel-Moss@users.noreply.github.com>
2025-10-08 13:52:10 +0100

    add imports for SVM model

---
+++ ai/ml-svm.py
@@ -0,0 +1,26 @@
+"""
+File: ml-svm.py
+Author: Sam, 
+Created: 2025-10-08
+Last Updated: 2025-10-08
+
+Description:
+    Support Vector Machines (SVM) Alogrithm for Human Activity Recognition Task
+
+Usage:
+    python ml-svm.py
+
+Notes:
+    - Completion TBC
+    - Peer Review TBC
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split 
+from sklearn import metrics 
+## Imports for Support Vector Machines ##
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
