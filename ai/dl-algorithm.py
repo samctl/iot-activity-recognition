00596487b65d021cbdd75d15b9d881ef1efa6fff
Samuel Moss <146960635+Samuel-Moss@users.noreply.github.com>
2025-11-01 11:52:47 +0000

    RNN algorithm imports

--- ai/dl-algorithm.py
+++ ai/dl-algorithm.py
@@ -11,6 +11,20 @@ Usage:
     python dl-algorithm.py
 
 Notes:
-    - This is a placeholder file
-    - Need to decide on a deep learning algorithm
+    - RNN Alogrithm
 """
+
+import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Below are the RNN deep learning libraries 
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
